df08c42fecc13bb14be57d2e4fff35190cad786f
zrshishir <zrshishir@gmail.com>
2022-01-19 21:52:58 +0600

    break statement

--- LanguagePractice/4.ControlFlow/43range/431range.py
+++ LanguagePractice/4.ControlFlow/43range/431range.py
@@ -3,3 +3,11 @@ for i in range(5):
 
 print(list(range(5)))
 print(list(range(5, 10)))
+print(list(range(0, 10, 3)))
+print(list(range(-10, -100, -30)))
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+    print(i, a[i])
+    
+print(sum(range(5)))
