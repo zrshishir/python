0e288b20b45e5df0a57ac8ce5c654f8a7b3d4250
zrshishir <zrshishir@gmail.com>
2022-01-20 09:12:51 +0600

    list operation

---
+++ LanguagePractice/5DataStructures/51MoreOnLists/511listExample.py
@@ -0,0 +1,12 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+
+print(fruits.count('apple'))
+print(fruits.index('pear'))
+fruits.reverse()
+print(fruits)
+fruits.append('mango')
+print(fruits)
+fruits.sort()
+print(fruits)
+fruits.pop()
+print(fruits)
