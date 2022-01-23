aebe52c44952ed9232720e7d9d46de14f0bba04e
zrshishir <zrshishir@gmail.com>
2022-01-23 09:31:33 +0600

    matrix initialization

---
+++ ProgrammingBook/1Introduction/12FrequentErrors/123MattrixInitails.py
@@ -0,0 +1,8 @@
+# matrix initialization
+
+M1 = [[0] * 2 for _ in range(2)]
+print('Square Mmatrix: ',M1)
+
+# another way to declare
+M2 = [[j for j in range(4)] for i in range(3)]
+print('Matrix: ', M2)
