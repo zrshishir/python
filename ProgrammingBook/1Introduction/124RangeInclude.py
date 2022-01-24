d7cd0bb80da1ab4c304dcde81c9b614148d0a24e
zrshishir <zrshishir@gmail.com>
2022-01-24 09:45:56 +0600

    range reverse

--- ProgrammingBook/1Introduction/124RangeInclude.py
+++ ProgrammingBook/1Introduction/124RangeInclude.py
@@ -1,6 +1,6 @@
 # it will print 0 to 9 excluded 10
-# for i in range(10):
-#     print(i)
+for i in range(10):
+    print(i)
 
 # it will not excluded 10 with reverese order
 for i in range(10, -1, -1):
