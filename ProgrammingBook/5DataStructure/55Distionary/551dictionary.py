81554fb8ecc881bdc45a267add28518ebd5e5500
zrshishir <zrshishir@gmail.com>
2022-01-27 09:09:26 +0600

    dictionary data types

---
+++ ProgrammingBook/5DataStructure/55Distionary/551dictionary.py
@@ -0,0 +1,26 @@
+tel = {'jack': 4098, 'sape': 4139}
+tel['guido'] = 4127
+print(tel)
+print(tel['jack'])
+
+del tel['sape']
+print(tel)
+
+tel['irv'] = 4127
+print(tel)
+
+print(list(tel))
+
+print(sorted(tel))
+
+print('guido' in tel)
+
+print('jack' not in tel)
+
+print(dict([('sape', 4139), ('guido', 4127), ('jack', 4098)]))
+
+arbitraryDict = {x: x**2 for x in (2, 4, 6)}
+print(arbitraryDict)
+
+stringKeyPair = dict(sape=4139, guido=4127, jack=4098)
+print(stringKeyPair)
