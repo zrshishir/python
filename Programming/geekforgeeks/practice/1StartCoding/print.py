8ced1bf28d8dbfa1962d49b00254e912d1e4d7c4
zrshishir <zrshishir@gmail.com>
2022-01-31 23:35:55 +0600

    hello world

---
+++ Programming/geekforgeeks/practice/1StartCoding/print.py
@@ -0,0 +1,13 @@
+def print_fun():
+    print("Hello World")
+    # Your code here
+    # Please follow proper indentation
+
+#{ 
+#Driver Code Starts.
+
+def main():
+    print_fun()
+
+if __name__ == '__main__':
+    main()
