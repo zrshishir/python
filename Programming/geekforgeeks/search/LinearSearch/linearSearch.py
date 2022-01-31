af4cb0ea455c5106b8e2339599eec4f5f9fafc57
zrshishir <zrshishir@gmail.com>
2022-01-31 10:30:50 +0600

    linear search

--- Programming/geekforgeeks/search/LinearSearch/linearSearch.py
+++ Programming/geekforgeeks/search/LinearSearch/linearSearch.py
@@ -1,6 +1,16 @@
-arr = [1,2,3,4]
-x = 5
-if x in arr:
-    print(arr.index(5))
-else:
-    print('not found')
+import sys
+
+
+def search(arr, value):
+    if value in arr:
+        return arr.index(value)
+    else:
+        return -1
+    
+noOfInput = int(sys.stdin.readline())
+
+for i in range(0, noOfInput):
+    arraySize = int(sys.stdin.readline())
+    inputArray = list(map(int, sys.stdin.readline().split()))
+    searchedValue = int(sys.stdin.readline())
+    print(search(inputArray, searchedValue))
