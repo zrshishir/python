d55b65ecab377c95b1a3ed76a99fd9e89fa4dbab
zrshishir <zrshishir@gmail.com>
2022-02-04 14:31:12 +0600

    operators

---
+++ Programming/geekforgeeks/practice/10Opereators/README.md
@@ -0,0 +1,5 @@
+# Resources: 
+    https://practice.geeksforgeeks.org/problems/operators-in-python/1/?track=fork-python&batchId=119
+
+#### Status: Accepted
+    
--- Programming/geekforgeeks/practice/8SplitTheInput/main.py
+++ Programming/geekforgeeks/practice/8SplitTheInput/main.py
@@ -1,21 +1,18 @@
-def print_func():
-       ## print ("Geeks Classes")
-    print ("Geeks For Geeks")
-    '''print ("Geeks Quiz")
-    print ("Geeks")
-    print ("Geeks For")'''
+def inPutS():
+    string = input() ## input in a single line()
+    s, i, f = string.split()## split the a into three parts: string, integer, and f. 
+    print(s + " " + str(int(i) + float(f))) ##type cast i to int, f to float. Add i with f. Typecast the result to string
 
 #{ 
-#Driver Code Starts.
+#  Driver Code Starts
 
-#Python code to use comment
-
-# Driver Code
 def main():
-    
-    print_func()
+    testcases=int(input()) #testcases
+    while(testcases>0):
+        inPutS() #the function that gets things done
+        testcases-=1
         
 
-if __name__ == '__main__':
+
+if __name__=='__main__':
     main()
-#} Driver Code Ends
