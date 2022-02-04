4db50ee3eacf5f5a255b240321c191aca6a9ecee
zrshishir <zrshishir@gmail.com>
2022-02-04 13:59:09 +0600

    concatenated string

---
+++ Programming/geekforgeeks/practice/5ConcatenateString/README.md
@@ -0,0 +1,5 @@
+# Resources: 
+    https://practice.geeksforgeeks.org/problems/concatenating-strings-python/1/?track=fork-python&batchId=119
+
+#### Status: Accepted
+    
---
+++ Programming/geekforgeeks/practice/5ConcatenateString/main.py
@@ -0,0 +1,15 @@
+def print_fun(string1, string2):
+    print (string1 + string2)
+
+def main():
+    testcases = int(input())
+    
+    while(testcases > 0):
+        string1 = input()
+        string2 = input()
+        print_fun(string1, string2)
+        
+        testcases -= 1
+
+if __name__ == '__main__':
+    main()
