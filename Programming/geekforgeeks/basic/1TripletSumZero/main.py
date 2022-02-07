bd4c87b413f06cce7fce371a21e9112c68e829ef
zrshishir <zrshishir@gmail.com>
2022-02-07 10:30:15 +0600

    triplet sum on array zero solved

--- Programming/geekforgeeks/basic/1TripletSumZero/main.py
+++ Programming/geekforgeeks/basic/1TripletSumZero/main.py
@@ -1,24 +1,13 @@
-#User function Template for python3
-
 class Solution:
     #Function to find triplets with zero sum.    
     def findTriplets(self, arr, n):
         found = False
         for i in range(0, n-2):
-        
             for j in range(i+1, n-1):
-            
-                for k in range(j+1, n):
-                
-                    if (arr[i] + arr[j] + arr[k] == 0):
-                        print(arr[i], arr[j], arr[k])
-                        found = True
-        
-                
-        # If no triplet with 0 sum
-        # found in array
-        if (found == False):
-            print(" not exist ")
+                for k in range(j+1, n):   
+                    if (arr[i] + arr[j] + arr[k] == 0):   
+                        return True
+        return found
 
 #{ 
 #  Driver Code Starts
