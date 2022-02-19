537852af66cf6042c77814b1e1ab9b2e86caedb0
zrshishir <zrshishir@gmail.com>
2022-02-20 04:38:43 +0600

    dynamic2

--- ProgrammingBook/1Introduction/16Techniques/165DynamicFibo.py
+++ ProgrammingBook/1Introduction/16Techniques/165DynamicFibo.py
@@ -1,4 +1,13 @@
-def fibo_naive(n): 
+def FiboDP(n): 
     if n <= 1:
         return n
-    return fibo_naive(n - 1) + fibo_naive(n - 2)
+    return FiboDP(n - 1) + FiboDP(n - 2)
+
+def FiboDPMem(n):
+    mem = [0, 1]
+    for i in range(2, n + 1):
+        mem.append(mem[-2] + mem[-1])
+    return mem[-1]
+
+print(FiboDP(32))
+print(FiboDPMem(32))
