d650220c716e7db33ee2f7a473d905f4534509d0
zrshishir <zrshishir@gmail.com>
2022-02-21 05:23:16 +0600

    started sweap line problem

---
+++ ProgrammingBook/1Introduction/16Techniques/162SweapLine/README.md
@@ -0,0 +1,2 @@
+## Resources: 
+    https://prologin.org/train/2012/semifinal/retour_vers_le_futur
---
+++ ProgrammingBook/1Introduction/16Techniques/162SweapLine/sweap_line.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+
+class Solution:
+    #Complete the below function
+    def sweapLine(S):
+        return S
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+import math
+
+def main():
+        T=int(input())
+        print(T)
+        while(T>0):
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
