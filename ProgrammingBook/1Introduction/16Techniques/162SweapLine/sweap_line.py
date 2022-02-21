c3605331e901a344dc70b3e58a90b6ad7435b2cc
zrshishir <zrshishir@gmail.com>
2022-02-21 06:06:24 +0600

    started sweap line problem

--- ProgrammingBook/1Introduction/16Techniques/162SweapLine/sweap_line.py
+++ ProgrammingBook/1Introduction/16Techniques/162SweapLine/sweap_line.py
@@ -1,21 +1,35 @@
 #User function Template for python3
 
+from turtle import right
+
+
 class Solution:
     #Complete the below function
-    def sweapLine(S):
-        return S
+    def maxIntervalIntersect(S):
+        B = ([(left, +1) for left in S] + [(right, -1) for right in S])
+        B.sort()
+        
+        c = 0
+        best = (c, None)
+        for x, d in B:
+            c += d
+            print(c, d)
+            if best[0] < c:
+                best = (c, x)
+        return best
 
 #{ 
 #  Driver Code Starts
 #Initial Template for Python 3
 
 
-import math
 
 def main():
         T=int(input())
         print(T)
         while(T>0):
+            x, y = map(int, input().split())
+            print(Solution.maxIntervalIntersect({x, y}))
             T-=1
 
 
