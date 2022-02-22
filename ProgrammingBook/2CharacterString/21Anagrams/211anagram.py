0a276c88fca355297e581eb3b13625a23dd8c89a
zrshishir <zrshishir@gmail.com>
2022-02-22 09:25:21 +0600

    complete the anagram problem

--- ProgrammingBook/2CharacterString/21Anagrams/211anagram.py
+++ ProgrammingBook/2CharacterString/21Anagrams/211anagram.py
@@ -1,17 +1,22 @@
-
 from re import S
 
 
 def Anagrams(S):
     d = {}
     for word in S:
-        print(word)
         s = ''.join(sorted(word))
         if s in d:
             d[s].append(word)
         else:
             d[s] = [word]
     return [d[s] for s in d if len(d[s]) > 1]
-# S = 'below elbow bowel'
-S = 'below the car is a rat drinking cider and bending its elbow while this thing is an arc that can act like a cat which cried during the night caused by pain in its bowel'
-(Anagrams(S))
+
+def main():
+        T=int(input())
+        while(T>0):
+            S = [x for x in input().strip().split()]
+            T-=1
+        print(Anagrams(S))
+        
+if __name__ == "__main__":
+    main()
