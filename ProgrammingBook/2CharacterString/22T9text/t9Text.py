b1c5e00088c49991934559ee9b40c94b4aee3cb9
zrshishir <zrshishir@gmail.com>
2022-02-22 15:54:02 +0600

    t9 text conversion

---
+++ ProgrammingBook/2CharacterString/22T9text/t9Text.py
@@ -0,0 +1,22 @@
+
+def T9Text(S):
+    d = {}
+    for word in S:
+        s = ''.join(sorted(word))
+        if s in d:
+            d[s].append(word)
+        else:
+            d[s] = [word]
+    return [d[s] for s in d if len(d[s]) > 1]
+
+def main():
+        T = int(input())
+        
+        while(T > 0):
+            S = input()
+            # S = [x for x in input().strip().split()]
+            T-=1
+        print((S))
+        
+if __name__ == "__main__":
+    main()
