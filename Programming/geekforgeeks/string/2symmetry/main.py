f110ccf727d96f8df447550aec8d68c1cd62d5dc
zrshishir <zrshishir@gmail.com>
2022-03-06 10:58:01 +0600

    starting symmmetry

---
+++ Programming/geekforgeeks/string/2symmetry/main.py
@@ -0,0 +1,29 @@
+def Palindrome(s):
+    last = len(s) - 1
+    mid = last // 2
+    start = 0
+    flag = 0
+    
+    while start <= mid:
+        if s[start] == s[last]:
+            start += 1
+            last -= 1
+        else:
+            flag = 1
+            break
+        
+    return flag
+
+def main():
+        T = int(input())
+        print(T)
+        while(T > 0):
+            S = input()
+            if (Palindrome(S)):
+                print("This is not palindrome")
+            else:
+                print("This is palindrome")
+            T -= 1
+        
+if __name__ == "__main__":
+    main()
