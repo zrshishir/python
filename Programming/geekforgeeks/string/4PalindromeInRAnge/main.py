76010b7538e986f6ff0516b44471edafce8658ac
zrshishir <zrshishir@gmail.com>
2022-03-23 17:52:31 +0600

    range of palindrome started

---
+++ Programming/geekforgeeks/string/4PalindromeInRAnge/main.py
@@ -0,0 +1,18 @@
+def IsPalindrome(s):
+    
+        
+    return 
+
+def main():
+        T = int(input())
+        print(T)
+        while(T > 0):
+            S = input()
+            if (IsPalindrome(S)):
+                print("This is not IsPalindrome")
+            else:
+                print("This is IsPalindrome")
+            T -= 1
+        
+if __name__ == "__main__":
+    main()
