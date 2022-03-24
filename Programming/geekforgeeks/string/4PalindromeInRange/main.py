53a76106b3b61166632f73c3fe9766e567a923e6
zrshishir <zrshishir@gmail.com>
2022-03-25 01:51:55 +0600

    testing input

--- Programming/geekforgeeks/string/4PalindromeInRange/main.py
+++ Programming/geekforgeeks/string/4PalindromeInRange/main.py
@@ -2,19 +2,23 @@ from unittest import result
 
 
 def IsPalindrome(number):
-    return 10 ** 0
+    
     storeNo = number
     result = 0
+    i = 0
     while number / 10 > 0:
-        result = result + (number % 10)
-    return 13 / 10
+        print(result, number, i)
+        result = result * (10 ** i) + (number % 10)
+        number = int(number / 10)
+        i += 1
+    return result
 
 def main():
     T = int(input())
     print(T)
     while(T > 0):
         lowerRange, upperRange = map(int, input().split())
-        print(IsPalindrome(lowerRange))
+        print(IsPalindrome(134))
         # while lowerRange <= upperRange:
             
         #     lowerRange += 1
