3ac1db839a39f6a7ceed3d7367c1b97526eb2eb0
zrshishir <zrshishir@gmail.com>
2022-03-25 19:59:37 +0600

    palindrome in range done

--- Programming/geekforgeeks/string/4PalindromeInRange/main.py
+++ Programming/geekforgeeks/string/4PalindromeInRange/main.py
@@ -1,27 +1,23 @@
-from unittest import result
-
-
 def IsPalindrome(number):
-    
     storeNo = number
     result = 0
     i = 0
-    while number / 10 > 0:
-        print(result, number, i)
-        result = result * (10 ** i) + (number % 10)
+    while number > 0:
+        result = result * 10 + (number % 10)
         number = int(number / 10)
         i += 1
-    return result
+    return result == storeNo
+
 
 def main():
     T = int(input())
-    print(T)
+    
     while(T > 0):
         lowerRange, upperRange = map(int, input().split())
-        print(IsPalindrome(134))
-        # while lowerRange <= upperRange:
-            
-        #     lowerRange += 1
+        while lowerRange <= upperRange:
+            if IsPalindrome(lowerRange):
+                print(lowerRange, end =" ")
+            lowerRange += 1
         T -= 1
         
 if __name__ == "__main__":
