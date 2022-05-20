3113471c37328f8942547b2224b5798e33267f11
zrshishir <zrshishir@gmail.com>
2022-05-20 22:21:44 +0600

    simple edit

--- Programming/geekforgeeks/practice/7TakingInput/main.py
+++ Programming/geekforgeeks/practice/7TakingInput/main.py
@@ -5,19 +5,14 @@ def inPutCat():
     print(a, b, c) ## comma is used as it automatically separates the variables by a space. 
     ## + can also be used to concatenate but it would require manual spaces to print the words with spaces between them.
 
-
 #{ 
 #Driver Code Starts.
 
-
-
 def main():
     testcases=int(input()) #testcases
-    while(testcases>0):
+    while (testcases > 0) :
         inPutCat() #the function that gets things done
-        testcases-=1
-        
-
+        testcases -= 1
 
 if __name__=='__main__':
     main()
