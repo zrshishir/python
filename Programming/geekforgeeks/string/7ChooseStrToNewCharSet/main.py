93006a378133a9ce2b56065256a6c2cdf6002854
zrshishir <zrshishir@gmail.com>
2022-06-26 23:48:41 +0600

    char conversion done

--- Programming/geekforgeeks/string/7ChooseStrToNewCharSet/main.py
+++ Programming/geekforgeeks/string/7ChooseStrToNewCharSet/main.py
@@ -1,6 +1,18 @@
+from string import ascii_lowercase
+
+
 def conversion(char_set: str, string: str)->str:
-    print(char_set, string)
-    return True
+    # hasing for new char set 
+    hash_char = dict()
+
+    for alphabet, new_char in zip(ascii_lowercase, char_set):
+        hash_char[new_char] = alphabet
+    
+    string1 = ''
+    for i in string:
+        string1 += hash_char[i]
+    
+    return string1
 
 def main():
     T = int(input())
@@ -8,7 +20,7 @@ def main():
     while(T > 0):
         char_set = input()
         string = input()
-        conversion(char_set, string)
+        print(conversion(char_set, string))
         T -= 1
         
 if __name__ == "__main__":
